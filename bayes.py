eca0766a2f14968cb3cf23da4f1f3454d9300507
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-10-03 16:35:45 -0700

    added search areas and probability

--- bayes.py
+++ bayes.py
@@ -21,3 +21,18 @@ class Search():
             print("Map file not found or could not be loaded {}".format(MAP_FILE, file=sys.stderr))
             sys.exit(1)
         
+        self.area_actual = 0
+        self.sailor_actual = [0,0] 
+
+        self.sa1 = self.img[SA1_CORNERS[1] : SA1_CORNERS[3], SA1_CORNERS[0] : SA1_CORNERS[2]]
+        self.sa2 = self.img[SA2_CORNERS[1] : SA2_CORNERS[3], SA2_CORNERS[0] : SA2_CORNERS[2]]
+        self.sa3 = self.img[SA3_CORNERS[1] : SA3_CORNERS[3], SA3_CORNERS[0] : SA3_CORNERS[2]]
+
+        self.p1 = 0.2
+        self.p2 = 0.5
+        self.p3 = 0.3
+
+        self.sep1 = 0
+        self.sep2 = 0
+        self.sep3 = 0
+        
